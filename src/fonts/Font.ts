653c6428164ae459f2f9b2330283491071b75ece
Thyago Ribeiro <70301260+thyago608@users.noreply.github.com>
2021-04-09 01:14:47 -0300

    Mudanças no layout

--- src/fonts/Font.ts
+++
@@ -1,5 +0,0 @@
-import { css } from "styled-components";
-
-export const DrukWide = css`
-  @import url("https://db.onlinewebfonts.com/c/2206d6cc490084998d531e8c1b2cbb4a?family=Druk+Wide+Bold");
-`;
